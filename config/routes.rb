b926898e0e06d1953ffc5aed8bac08e12343a40f
root <U-Masaking>
2021-08-08 05:46:46 +0000

    login and logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
   root 'static_pages#home'
   get 'home' => "static_pages#home"
   get 'help' => "static_pages#help"
@@ -7,6 +8,9 @@ Rails.application.routes.draw do
   get 'contact' => "static_pages#contact"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'signup' => "users#new"
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
   resources :users
 
 end
